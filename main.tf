3e4c8a77dc8bbff5b24755e7cb5cf7def481b59c
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-04-20 12:04:38 +0530

    fixed-response

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ resource "aws_lb_listener" "main" {
   # certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
 
   default_action {
-    type = "fixed_response"
+    type = "fixed-response"
     fixed_response {
       content_type = "text/plain"
       message_body = "Unauthorized"
